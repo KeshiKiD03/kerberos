b6a4e2693f56c65dd9f2350fdbab71c2fb575346
Aaron <91669694+KeshiKiD03@users.noreply.github.com>
2022-03-09 22:32:00 +0100

    Update startup.sh

--- Practica2 - KHOSTLDAP/khostp22 - OLDKESHI/startup.sh
+++ Practica2 - KHOSTLDAP/khostp22 - OLDKESHI/startup.sh
@@ -6,8 +6,8 @@
 # Copiar arxius necesaris
 
 cp /opt/docker/krb5.conf /etc/krb5.conf 	# Sobreescrivim els fitxers per els nostres.
-cp /opt/docker/kdc.conf /etc/krb5kdc/kdc.conf
-cp /opt/docker/kadm5.acl /etc/krb5kdc/kadm5.acl
+#cp /opt/docker/kdc.conf /etc/krb5kdc/kdc.conf
+#cp /opt/docker/kadm5.acl /etc/krb5kdc/kadm5.acl
 
 
 cp /opt/docker/common-auth /etc/pam.d/common-auth
@@ -15,7 +15,7 @@ cp /opt/docker/common-account /etc/pam.d/common-account
 cp /opt/docker/common-password /etc/pam.d/common-password
 cp /opt/docker/common-session /etc/pam.d/common-session
 
-kdb5_util create -s -P masterkey # Crea la BD con Masterkey
+#kdb5_util create -s -P masterkey # Crea la BD con Masterkey
 
 # Añade grupo local
 
@@ -40,7 +40,7 @@ echo -e "local01\nlocal01" | passwd local01
 echo -e "local02\nlocal02" | passwd local02
 echo -e "local03\nlocal03" | passwd local03
 
-/etc/init.d/krb5-admin-server start
-/etc/init.d/krb5-kdc start
+#/etc/init.d/krb5-admin-server start
+#/etc/init.d/krb5-kdc start
 
 /bin/bash
