0d7dee53d737e94da0244cd185b4ddebeee178d9
KeshiKiD03 <aaronandal03@gmail.com>
2022-03-09 09:26:58 +0100

    Wed 09 Mar 2022 09:26:58 AM CET

--- Practica2 - KHOSTLDAP/khostldap22/startup.sh
+++ Practica2 - KHOSTLDAP/khostldap22/startup.sh
@@ -9,7 +9,11 @@ cp /opt/docker/krb5.conf /etc/krb5.conf 	# Sobreescrivim els fitxers per els nos
 cp /opt/docker/kdc.conf /etc/krb5kdc/kdc.conf
 cp /opt/docker/kadm5.acl /etc/krb5kdc/kadm5.acl
 
-
+cp /opt/docker/ldap.conf /etc/ldap/ldap.conf
+cp /opt/docker/login.defs /etc/login.defs
+cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
+cp /opt/docker/nslcd.conf /etc/nslcd.conf
+cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
 cp /opt/docker/common-auth /etc/pam.d/common-auth
 cp /opt/docker/common-account /etc/pam.d/common-account
 cp /opt/docker/common-password /etc/pam.d/common-password
@@ -40,7 +44,14 @@ echo -e "local01\nlocal01" | passwd local01
 echo -e "local02\nlocal02" | passwd local02
 echo -e "local03\nlocal03" | passwd local03
 
+/usr/sbin/nscd
+/usr/sbin/nslcd
+
 /etc/init.d/krb5-admin-server start
 /etc/init.d/krb5-kdc start
+
+# Asegurar que funcione
+getent passwd
+getent group
 
 /bin/bash
